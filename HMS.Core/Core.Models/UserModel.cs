6076ee6417265dd01af05b51dfcb2b2cac802e2c
Niccolo Peretti <peretti@dylog.it>
2022-07-17 17:40:27 +0200

    added UserModelBase

---
+++ HMS.Core/Core.Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS
+{
+    public abstract class UserModel
+    {
+        #region properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FiscalCode { get; set; }  
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; } 
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserId { get; set; }
+        public DateTime Created { get; set; }
+        #endregion
+    }
+}
