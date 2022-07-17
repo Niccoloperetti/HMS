41703d6ea7f2d42a507381879674f1d8f96477da
Niccolo Peretti <peretti@dylog.it>
2022-07-17 18:17:18 +0200

    added AdminUserModel

---
+++ HMS.Core/Core.Models/AdminUserModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace HMS
+{
+    public class AdminUserModel: UserModelBase
+    {
+        #region properties
+        public string? AdminID { get; set; }
+        public int? AdminLevel { get; set; }
+        #endregion
+
+        #region constructor
+        public AdminUserModel() : base() {}
+        public AdminUserModel(string? name, string? surname, string? fiscalCode, DateTime? birthday, string? address,
+            string? email, string? phoneNumber, string? adminID, int? adminLevel) : base(name, surname, fiscalCode, birthday, address, email, phoneNumber)
+        {
+            AdminID = adminID;
+            AdminLevel = adminLevel;
+        }
+
+        #endregion
+    }
+}
